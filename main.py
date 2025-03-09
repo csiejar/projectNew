a10fb69893a3e8d7fb37162d1b5b027c90e7f670
csiejar <j0970238552@gmail.com>
2025-03-09 10:50:00 +0800

    carousel v1

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-# from google.auth.transport import requests
-# from google.oauth2 import id_token
+from google.auth.transport import requests
+from google.oauth2 import id_token
 
 app = FastAPI(debug=True)
 
@@ -34,19 +34,19 @@ CLIENT_ID = "1071265157577-7bcbr8qejtvtbmnchgs06ohr9ke46v97.apps.googleuserconte
 
 class TokenData(BaseModel):
     token: str
-# @app.post("/verify-token")
-# async def verify_token(data: TokenData):
-#     try:
-#         # 驗證 ID Token
-#         id_info = id_token.verify_oauth2_token(data.token, requests.Request(), CLIENT_ID)
-#         # 取得用戶資訊
-#         googleID = id_info["sub"]
-#         email = id_info["email"]
-#         name = id_info.get("name", "未知用戶")
-#         return {"googleID": googleID, "email": email, "name": name}
+@app.post("/verify-token")
+async def verify_token(data: TokenData):
+    try:
+        # 驗證 ID Token
+        id_info = id_token.verify_oauth2_token(data.token, requests.Request(), CLIENT_ID)
+        # 取得用戶資訊
+        googleID = id_info["sub"]
+        email = id_info["email"]
+        name = id_info.get("name", "未知用戶")
+        return {"googleID": googleID, "email": email, "name": name}
     
-#     except Exception as e:
-#         raise HTTPException(status_code=401, detail=f"驗證失敗: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=f"驗證失敗: {str(e)}")
 
 # 啟動服務
 if __name__ == "__main__":
